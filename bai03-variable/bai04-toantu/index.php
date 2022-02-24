66b8c3badddf69b124ff2c0a5f814a4296b4ed12
uyenvy123 <100343535+uyenvy123@users.noreply.github.com>
2022-02-24 16:51:19 +0700

    Add files via upload

---
+++ bai03-variable/bai04-toantu/index.php
@@ -0,0 +1,30 @@
+<?php
+    $a = 5;
+    $b = 3;
+    $Tong = $a + $b;
+    echo "Tong: $Tong <br>";
+
+    $Hieu = $a - $b;
+    echo "Hieu: $Hieu <br>";
+
+    $Tich = $a * $b;
+    echo "Tich: $Tich <br>";
+
+    $Thuong = $a / $b;
+    echo "Thuong: $Thuong <br>";
+
+    $Sodu = $a % $b;
+    echo "Sodu: $Sodu <br>";
+  
+    $ss_bang = $a == $b;
+    echo "so sanh: $ss_bang <br>";
+
+    $ss_khac = $a != $b;
+    echo "so sanh khac: $ss_khac <br>";
+
+    $ss_lon_hon = $a > $b;
+    echo "so sanh lon hon: $ss_lon_hon <br>"; 
+?>
+
+
+
