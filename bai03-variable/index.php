d8b09b08d14eaa7dd8938aabe30e17198162b3ae
uyenvy123 <100343535+uyenvy123@users.noreply.github.com>
2022-02-24 16:21:35 +0700

    Add files via upload

---
+++ bai03-variable/index.php
@@ -0,0 +1,22 @@
+<?php
+  $a = 5;
+ echo $a;
+ $str = "Hello World";
+ echo $str; 
+    $firstName = 'Nguyen';
+    $lastName = 'Van An';
+    $fullName = $firstName . ' '. $lastName;
+    echo $fullName; 
+     
+    $x = 10.365;
+    var_dump($x); 
+    $cars = array('Toyota', 'BMW', 'Volvo');
+    var_dump($cars);
+    $a = 5.6; 
+    $b= $a . 'hello';
+    var_dump($b);
+
+    $x = " Hello world!";
+    $x = null;
+    var_dump($x);
+?>
